a1114cd7a3911fd9279c5fea6912c30ea11101ab
Kasey Cushing <kasey.cushing@gmail.com>
2020-10-14 22:24:35 -0400

    local storage saves score and posts score to page, submit button active but not saving initials

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let btnSelected = document.querySelector(".btn")
 let responseMsg = document.querySelector("#responseHere")
 let scoreCardForm = document.querySelector(".scorecard")
 let accessScoreCard = document.querySelector(".clickscores")
-let initials = document.querySelector("#initials")
+let name = document.querySelector("#name")
 let submitBtn = document.querySelector("#submitBtn")
 
 const theQuestions =[
@@ -62,9 +62,7 @@ function setTime(){
       theCard.style.display = "none"
       window.location.href="./highscore.html"
     }
-  }, 1000);
-
-  console.log("timer working")
+  }, 3000);
 }
 
 startBtn.addEventListener("click", function(){
@@ -72,7 +70,6 @@ startBtn.addEventListener("click", function(){
   timeEl.style.display = "block";
   setTime();
   generateQuiz();
-  console.log("start button working")
 })
 
 function generateQuiz(){
@@ -87,6 +84,7 @@ function generateQuiz(){
   let wrongAnswer = document.createElement("p")
   let correctAnswer = document.createElement("p") 
 
+  //view question at top of card
   function showQuestion(){
       let listQuestion = theQuestions[questionIndex].question
       placeQuestion.textContent = listQuestion
@@ -94,6 +92,7 @@ function generateQuiz(){
          
     console.log("showQs is connected")
   
+    //show answers as buttons
     function showAnswers(){
       let answerA = theQuestions[questionIndex].answer[0].list
       let answerB = theQuestions[questionIndex].answer[1].list
@@ -107,8 +106,6 @@ function generateQuiz(){
       multChoiceB.appendChild(btnTextB)
       multChoiceC.appendChild(btnTextC)
       multChoiceD.appendChild(btnTextD)
-
-      console.log("showAnswers is connected")
     }
 
   showAnswers()
@@ -120,14 +117,23 @@ function generateQuiz(){
     let wrongText = "Wrong Answer!"
     wrongAnswer.textContent = wrongText
     responseMsg.appendChild(wrongAnswer)
+
+    setTimeout(function(){
+      responseMsg.removeChild(wrongAnswer)
+    }, 1500)
   }
 
   let correctMessage = function(){
     let correctText = "Correct Answer!"
     correctAnswer.textContent = correctText
     responseMsg.appendChild(correctAnswer)
+
+    setTimeout(function(){
+      responseMsg.removeChild(correctAnswer)
+    }, 1500)
   }
 
+    //the chosen answer promps correct or incorrect
   multChoiceA.addEventListener("click", function(){
     if (theQuestions[questionIndex].answer[0].response === false){
       wrongMessage()
@@ -136,8 +142,10 @@ function generateQuiz(){
       correctMessage()
       score ++
     }
-    resetCard()
-  })
+    setTimeout(function(){
+      resetCard()
+    }, 1500)
+
   multChoiceB.addEventListener("click", function(){
     if (theQuestions[questionIndex].answer[1].response === false){
       wrongMessage()
@@ -146,8 +154,11 @@ function generateQuiz(){
       correctMessage()
       score ++
     }   
-    resetCard()
+    setTimeout(function(){
+      resetCard()
+    }, 1500)
   })
+
   multChoiceC.addEventListener("click", function(){
     if (theQuestions[questionIndex].answer[2].response === false){
       wrongMessage()
@@ -156,8 +167,11 @@ function generateQuiz(){
       correctMessage()
       score ++
     }   
-    resetCard()
+    setTimeout(function(){
+      resetCard()
+    }, 1500)
   })
+
   multChoiceD.addEventListener("click", function(){
     if (theQuestions[questionIndex].answer[3].response === false){
       wrongMessage()
@@ -166,23 +180,25 @@ function generateQuiz(){
       correctMessage()
       score ++
     }   
-    resetCard()
+    setTimeout(function(){
+      resetCard()
+    }, 1500)
   })
 
+  //clear the card so it can reset with next question
   function resetCard(){
-    placeQuestion.parentNode.removeChild(placeQuestion)
-    btnTextA.parentNode.removeChild(btnTextA)
-    btnTextB.parentNode.removeChild(btnTextB)
-    btnTextC.parentNode.removeChild(btnTextC)
-    btnTextD.parentNode.removeChild(btnTextD)
-    console.log("reset is connected")
-
+      placeQuestion.parentNode.removeChild(placeQuestion)
+      btnTextA.parentNode.removeChild(btnTextA)
+      btnTextB.parentNode.removeChild(btnTextB)
+      btnTextC.parentNode.removeChild(btnTextC)
+      btnTextD.parentNode.removeChild(btnTextD)
+   
     nextQuestion()
   }
 
+  //pull the next question from theQuestions index and apply to card
   function nextQuestion(){
     questionIndex++
-    console.log("nextQ connected")
     if (theQuestions.length > questionIndex){
       showQuestion()
     } else {
@@ -191,21 +207,49 @@ function generateQuiz(){
 }
 
   function quizEnd(){
+    let finalScore = score
     secCountdown =- secCountdown
-    scoreCard()
+    localStorage.setItem("mostRecentScore", finalScore)
+    goToScoreCard()
     console.log("quizEnd is connected")
   }
 
- function scoreCard(){
+ function goToScoreCard(){
    window.location.href="./highscore.html"
-   //initials.addEventListener("click", function(){
-     //console.log(initials.value)
-   
-   console.log("scorecard function connected")
- //})
-  console.log("genQuiz is connected")
+ }
+
+})
 }
+
+//see works cited, James Q Quick
+//scorecard specific, names in local storage
+const highScoresList = document.querySelector("#highScoresList")
+let scorePosted = document.getElementById("scorePosted")
+const userInitials = form.name
+let mostRecentScore = localStorage.getItem("mostRecentScore") || []
+scorePosted.innerText = mostRecentScore
+const highScores = JSON.parse(localStorage.getItem("highscores"))
+
+submitBtn.addEventListener("click", function(event){
+  event.preventDefault()
+  let scoreKeep = {
+    name : userInitials,
+    score : mostRecentScore
+  }
+  //highScores.push(scoreKeep)
+
+  scoreCard()
+  console.log("submit button connected")
+  console.log(highScoresList)
+})
+
+function scoreCard(){
+  let newLi = document.createElement("li")
+  newLi.textContent = userInitials + " - " + mostRecentScore
+  highScoresList.appendChild(newLi)
+
 }
 
 
-  
+console.log(userInitials)
+
